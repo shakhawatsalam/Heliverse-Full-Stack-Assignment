fda9061d95ee0c25ec03760a9330bbca093c59af
shakhawatsalam <shakhawatsalam726@gmail.com>
2023-12-02 11:36:57 +0600

    user model and schama is created success fully

---
+++ src/app/module/use/user.interface.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/consistent-type-definitions */
+
+export type IUser = {
+  id: number;
+  first_name: string;
+  last_name: string;
+  email: string;
+  gender: string;
+  avatar: string;
+  domain: string;
+  available: boolean;
+};
---
+++ src/app/module/use/user.model.ts
@@ -0,0 +1,25 @@
+import mongoose, { Model, Schema } from "mongoose";
+import { IUser } from "./user.interface";
+
+const UserSchema: Schema<IUser> = new mongoose.Schema(
+  {
+    id: { type: Number, required: true },
+    first_name: { type: String, required: true },
+    last_name: { type: String, required: true },
+    email: { type: String, required: true },
+    gender: { type: String, required: true },
+    avatar: { type: String, required: true },
+    domain: { type: String, required: true },
+    available: { type: Boolean, required: true },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+    },
+  },
+);
+
+const userModel: Model<IUser> = mongoose.model("User", UserSchema);
+
+export default userModel;
